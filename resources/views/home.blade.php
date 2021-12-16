02d4cfa9427c501214a267249933ef43ad403e14
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2021-12-16 21:49:43 +0530

    customApp Layout to Main App Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -152,7 +152,7 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         @if (!Auth::user()->email_verified_at)
             <div class="row">
                 <div class="col s12">
