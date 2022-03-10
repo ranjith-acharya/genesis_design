d8c3353803f6b528a99d84a81f35dd9806153448
nikhiljagtap <nikhiljagtap702@gmail.com>
2022-03-10 18:15:13 +0530

    new Events

---
+++ app/Jobs/StripeWebhooks/HandleChargeRefundedJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs\StripeWebhooks;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+class HandleChargeRefundedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(WebhookCall $webhookCall)
+    {
+        $this->webhookCall = $webhookCall;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $response=$this->webhookCall->payload['data']['object'];
+        return $response;
+    }
+}
